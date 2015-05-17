a6bdb4f5331b5885a0e1150ed22c6705a7d1a786
mgcdanny <daniel.gabrieli@gmail.com>
2015-05-16 20:39:48 -0400

    users via secure cookies

--- chatdemo.py
+++ chatdemo.py
@@ -1,70 +1,92 @@
 import logging
+import tornado.httpserver
 import tornado.escape
 import tornado.ioloop
 import tornado.options
 import tornado.web
 import tornado.websocket
 import os.path
-import uuid
 from tornado.options import define, options
 
 from collections import defaultdict
 
-
 define("port", default=8888, help="run on the given port", type=int)
 
+
 class Application(tornado.web.Application):
+
     def __init__(self):
         handlers = [
-            (r'/', Dan),
-            (r'/room/(?P<room>.*)?', Dan),
-            (r'/chatws/(?P<room>.*)?', DanSocket)
+            (r'/', LogIn),
+            (r'/room/(?P<room>.*)?', Room),
+            (r'/chatws/(?P<room>.*)?', ChatWS),
         ]
         settings = dict(
             cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
             static_path=os.path.join(os.path.dirname(__file__), "static"),
-            xsrf_cookies=True
+            xsrf_cookies=True,
+            debug=True
         )
-        tornado.web.Application.__init__(self, handlers, debug=True, **settings)
+        super(Application, self).__init__(handlers, **settings)
 
 
-class Dan(tornado.web.RequestHandler):
+class LogIn(tornado.web.RequestHandler):
+
+    def get(self):
+        self.render("login.html")
+
+    def post(self):
+        self.set_header("Content-Type", "text/plain")
+        user = self.get_body_argument("user")
+        room = self.get_body_argument("room")
+        self.set_secure_cookie('user', user)
+        self.set_secure_cookie('room', room)
+        self.redirect('/room/{}'.format(room))
+
+
+class Room(tornado.web.RequestHandler):
+
     def get(self, **route_params):
-        self.render("index.html")
+        self.render("room.html")
 
-class DanSocket(tornado.websocket.WebSocketHandler):
-    #rooms is a dict with key the room name and value a set of webSockethandlers
+class ChatWS(tornado.websocket.WebSocketHandler):
+    #TODO: implennet new rooms dict structure
+    #rooms is a dict with key the room name and value a set of tuples (user, wsHandler)
+    # {'room': set(('user1': wsHandler1), ('user2': wsHandler2))
+
     rooms = defaultdict(set)
 
     def open(self, room):
         print("Dan Socket Opened")
-        DanSocket.rooms[room].add(self)
-        
+        ChatWS.rooms[room].add((self.get_secure_cookie('user'), self))
+        print(ChatWS.rooms)
+
 
     def on_close(self):
-        DanSocket.rooms[self.path_kwargs['room']].remove(self)
+        ChatWS.rooms[self.path_kwargs['room']].remove((self.get_secure_cookie('user'), self))
         print("Dan Socket Closed")
+
 
     def on_message(self, msg):
         parsed = tornado.escape.json_decode(msg)
         print(parsed)
-        DanSocket.send_updates(parsed)
+        ChatWS.send_updates(parsed)
 
     @classmethod
     def send_updates(cls, msg):
-        for web_socket in cls.rooms[msg['room']]:
+        for user, ws_handler in cls.rooms[msg['room']]:
             try:
-                print(web_socket)
-                web_socket.write_message(msg)
+                print(ws_handler)
+                ws_handler.write_message(msg)
             except:
                 logging.error("Error sending message", exc_info=True)
 
 
 def main():
     tornado.options.parse_command_line()
-    app = Application()
-    app.listen(options.port)
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
     tornado.ioloop.IOLoop.current().start()
 
 
